67163f6f550e339e8b52c779c99dd0fed43ce7de
jaganjavid <jagantutor1@gmail.com>
2023-03-07 14:53:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [name, setName] = useState("Javid")
+
+  const [events, setEvents] = useState([
+    {title: "Hey Enjoy react learning", id: 1},
+    {title: "Hey Josh How are you", id: 2},
+    {title: "Hey Jagan are you web dev", id: 3},
+  ])
+
+  const handelClick = () =>{
+    setName("Josh")
+    console.log(name);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>My name is {name}</h1>
+        <button onClick={handelClick}>Change name</button>
+
+        {
+          events.map((event) => (
+            <div key={event.id}>
+              <h2>{event.title}</h2>
+            </div>
+          ))
+        }
     </div>
   );
 }
