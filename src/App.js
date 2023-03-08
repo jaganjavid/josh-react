3f0cfa0065c7339154f35f159aa581f4d5823609
jaganjavid <jagantutor1@gmail.com>
2023-03-08 15:25:33 +0530

    react 2nd day

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { useState } from 'react';
 
 function App() {
 
-  const [name, setName] = useState("Javid")
+  const [name, setName] = useState("Javid");
+
+  const [showEvent, setShowEvent] = useState(true);
 
   const [events, setEvents] = useState([
     {title: "Hey Enjoy react learning", id: 1},
@@ -11,25 +13,44 @@ function App() {
     {title: "Hey Jagan are you web dev", id: 3},
   ])
 
-  const handelClick = () =>{
-    setName("Josh")
-    console.log(name);
+  // const handelClick = () =>{
+  //   setName("Josh")
+  //   console.log(name);
+  // }
+
+  // console.log(showEvent);
+
+  const deleteClick = (id) => {
+    setEvents((prevState) => {
+      console.log(prevState);
+      return prevState.filter((event) => (
+        event.id !== id
+      ))
+    })
   }
 
   return (
     <div className="App">
-        <h1>My name is {name}</h1>
-        <button onClick={handelClick}>Change name</button>
+        {/* <h1>My name is {name}</h1>
+        <button onClick={handelClick}>Change name</button> */}
+        <div>
+          {showEvent && <button onClick={() => {setShowEvent(false)}}>hide Event</button>}
+        </div>
 
-        {
-          events.map((event) => (
+        <div>
+          {!showEvent && <button onClick={() => {setShowEvent(true)}}>Show Event</button>}
+        </div>
+
+        {showEvent && events.map((event) => (
             <div key={event.id}>
               <h2>{event.title}</h2>
+              <button onClick={() => {deleteClick(event.id)}}>delete me</button>
             </div>
-          ))
-        }
+          ))}
     </div>
   );
 }
+
+
 
 export default App;
